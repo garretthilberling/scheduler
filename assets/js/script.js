4ea54d2dbc91a42660df33eeaa3c095fa2c2504b
garretthilberling <ghilberling@knights.ucf.edu>
2022-01-28 20:58:27 -0500

    names can be stored and functionality is mostly complete. Trying to do the time sensitive color coding now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
-var currentDate = moment().toDate();
+var currentDate = moment().format("dddd, MMM Do YYYY");
 $("#currentDay").append(currentDate);
+
+var currentTime = moment().format("h:mm:ss A");
+$("#currentTime").append("<span>").text(currentTime);
+
+
+var assignments = JSON.parse(localStorage.getItem("assignments")) || [];
+
+var input = $("#input").val();
 
 var timeSlotSection = $("#time-slot");
 
-var times = ['8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']
+var times = [];
+for (let hour = 8; hour <= 17; hour++) { //specifying the time will start at 8 and end at 5
+    times.push(moment({hour}).format("h:mm A")); //pushing times into array in a way that moment can understand.
+}
+var attrInput = ['input-1', 'input-2', 'input-3', 'input-4','input-5', 'input-6', 'input-7', 'input-8', 'input-9'];
 for (var i = 0; i < times.length; i++) {
-    var assmtDiv = $("<div>").addClass("col-1").text(times[i]).css({fontSize: 22});
-    var assmtInput = $("<input>").addClass("col-10 form-control").attr("id", "input").attr("type", "text").attr("placeholder","assignment");
-    var assmtSaveBtn = $("<btn>").addClass('col-1').attr("id", "savebtn")
+    var assmtDiv = $("<div>").addClass("col-1 px-1 bg-info badge text-white justify-content-*-around align-content-*-around").text(times[i]).css({fontSize: 18}).height('36px');
+    var assmtInput = $("<input>").addClass("col-10 form-control").attr("id", (attrInput[i])).attr("type", "text");
+    var assmtSaveBtn = $("<btn>").addClass('col-1');
     var assmtSaveIcon = $("<i>").css({fontSize: 18}).addClass("fa fa-save btn btn-primary");
     assmtSaveBtn.append(assmtSaveIcon);
     var assmtContainer = $("<div>").addClass("row m-2 p-2");
@@ -16,33 +28,44 @@ for (var i = 0; i < times.length; i++) {
 }
 
 
-var assignments = {};
+var saveBtns = document.getElementsByClassName('btn');
+
+for (var i = 0; i < saveBtns.length; i++) {
+    saveBtns[i].addEventListener('click', function(){
+       // grab the value from the input area
+       var inputVal = $(this).parent().parent().find('input').val();
+       var inputId = $(this).parent().parent().find('input').attr('id');
+      
+       assignments.push({
+          inputVal,
+          inputId
+       })
+
+      localStorage.setItem('assignments', JSON.stringify(assignments));
+    })
+}
 
 
 
+function load () {
+    for(var i = 0; i < assignments.length; i++) {
+        console.log(`#${assignments[i].inputId}`)
+        $(`#${assignments[i].inputId}`).val(assignments[i].inputVal);
+    }
+
+}
+
+function checkTime () {
+    for (var i = 0; i < times.length; i++) {
+      $(assmtInput[i]).parent.removeClass("bg-danger bg-warning");
+      if (times[i] <= currentTime) {
+        $(this).parent.addClass("bg-danger");
+      } else if (times[i] >= currentTime + 2) {
+        $(this).parent.addClass("bg-warning");
+      }
+    }
+}
 
 
-// var loadAssignments = function() {
-//     assignments = JSON.parse(localStorage.getItem("assignments"));
-
-//     if (!assignments) {
-//         assignments ={
-//             hour8: [], hour9: [], hour10: [], hour11: [], hour12: [], hour1: [], hour2: [], hour3: [], hour4: [], hour5: []
-//         };
-//     }
-    // for (let i = 0; i < assignments.length; i++) {
-    //     assignments.push(input);
-    // }
-// }
-
-// $("#savebtn").on("click", save);
-
-// var save = function (e) {
-//     assignments.push(input);
-
-    // localStorage.setItem("assignments", JSON.stringify(assignments));
-    // console.log(assignments);
-// }
-
-
-// loadAssignments();
+load();
+checkTime();
